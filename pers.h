d9570f03257dc768055414a785e4d53aa57ee918
Asher <4905338@gmail.com>
2024-08-12 14:57:57 +0300

    Initial && Created some classes in pers.h

---
+++ pers.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+class Entity
+{
+private:
+public:
+};
+
+
+
+class Player : public Entity
+{
+private:
+public:
+};
+
+
+
+class Bot : public Entity
+{
+private:
+protected:
+public:
+};
+
+
+
+class Enemy : public Bot
+{
+private:
+public:
+};
+
+
+
+class Friend : public Bot
+{
+private:
+public:
+};
