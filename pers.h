2d8311e0c12885db3ac6f85d08452479123360f8
Asher <4905338@gmail.com>
2024-08-12 15:30:35 +0300

    Added content to the entity class

--- pers.h
+++ pers.h
@@ -10,7 +10,22 @@ using namespace sf;
 class Entity
 {
 private:
+	float x, y;
+	float health, speed, stamine;
+	const float gravity = 0.0098f;
+	string File;
+	string FileRev; // for reversed animation
+	Texture texture;
+	Texture textureRev;
+	Sprite spriteEntity;
+	virtual void move() = 0;
+protected:
+	Entity(float h, float speed, float stamine, string File){};
+	enum statesOfPers = {stay, right, left, jump};
+	enum statesofJump = {no, down, up};
 public:
+	void Spawn(float x, float y);
+	void Update(const float &time);
 };
 
 
@@ -27,7 +42,6 @@ class Bot : public Entity
 {
 private:
 protected:
-public:
 };
 
 
